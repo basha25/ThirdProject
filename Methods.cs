f36843b5353f53ef249436382b2db4e55463b33c
RAGHUMAN BASHA A <ar13995@isolvegroup.org>
2023-10-16 12:56:50 +0530

    Inital comment

---
+++ Methods.cs
@@ -0,0 +1,16 @@
+using System;
+namespace MyApplication
+{
+  class Program
+  {
+    static void MyMethod()
+    {
+      Console.WriteLine("I just got executed!");
+    }
+
+    static void Main(string[] args)
+    {
+      MyMethod();
+    }
+  }
+}
